00ff351b7350d20e133905a67078da5001711e25
jorgemerasilva <jorgemeracontato@gmail.com>
2025-12-02 18:05:00 -0300

    update

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone iphone = new Iphone("75998338424", "Iphone 16 Pro Max", "ghh4f2yuio2yy2", 244221);
+Nokia nokia = new Nokia("75994332985", "Nokia 3 Max", "1751213517353", 244221);
+
+iphone.InstalarAplicativo("Facebook");
+iphone.ReceberLigacao();
+
+
+nokia.InstalarAplicativo("Instagram");
+nokia.ReceberLigacao();
